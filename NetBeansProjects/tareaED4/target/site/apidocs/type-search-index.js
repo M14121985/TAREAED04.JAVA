8fb60b3366cf0127b5f74c32dfc98e668ea41220
M14121985 <155565154+M14121985@users.noreply.github.com>
2024-01-27 02:15:00 +0100

    Add files via upload

---
+++ NetBeansProjects/tareaED4/target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.mycompany.tareaed4","l":"Cuenta1"},{"p":"com.mycompany.tareaed4","l":"Main"}];updateSearchResults();
